5046aa35c25d38a96d51cd4d876bd144b4bf93a5
arno06 <arno06@gmail.com>
2016-06-23 10:18:38 +0200

    Prise en compte des valeurs type number

--- src/Template.js
+++ src/Template.js
@@ -185,7 +185,7 @@ Class.define(Template, [EventDispatcher],
 							}
 							v = tmp.replace(key, t_0+(params[2]||"$v")+"."+c_key+t_1);
 							v = v.replace("$"+c_key, (params[2]||"$v")+"."+c_key);
-							if(typeof d[j] == "string")
+							if(typeof d[j] == "string" || typeof d[j] == "number")
 							{
 								tmp = d[j];
 								d[j] = {};
